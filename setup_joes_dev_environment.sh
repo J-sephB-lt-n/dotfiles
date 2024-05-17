38de74e72ef4fef218507f32d530767cc87a13b0
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-17 16:11:12 +0200

    fix(setup_joes_dev_environment.sh): updated github links

--- setup_joes_dev_environment.sh
+++ setup_joes_dev_environment.sh
@@ -1,9 +1,14 @@
+# USAGE: 
+# $ wget https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/setup_joes_dev_environment.sh
+# $ sudo bash setup_joes_dev_environment.sh
+# $ rm setup_joes_dev_environment.sh
+
 apt-get update
 apt-get install -y --no-install-recommends \
     bc fzf htop tmux tree vim wget
 
-wget https://raw.githubusercontent.com/J-sephB-lt-n/my-vim-config/main/.vimrc  
-mv .vimrc ~/.vimrc 
-wget https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-bashrc-zshrc/main/.bash_aliases
+wget https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.bash_aliases
 mv .bash_aliases ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.bashrc
+wget https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.vimrc  
+mv .vimrc ~/.vimrc 
