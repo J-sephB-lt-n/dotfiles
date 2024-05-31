ae6cf5cf545eba013875efbaabaa6baea8481b93
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-31 15:47:06 +0200

    fix(setup_joes_dev_environment.sh): fixed wget output paths

--- setup_joes_dev_environment.sh
+++ setup_joes_dev_environment.sh
@@ -11,18 +11,18 @@ apt-get update
 apt-get install -y --no-install-recommends \
     bc fzf htop tmux tree vim wget
 
-wget --directory-prefix=~ https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.bash_aliases
+wget -O ~/.bash_aliases https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.bash_aliases
 echo 'source ~/.bash_aliases' >> ~/.bashrc
-wget --directory-prefix=~ https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.vimrc  
+wget -O ~/.vimrc https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.vimrc  
 
 mkdir ~/cli_scripts
 for script_name in "gpt.py"; 
 do
-    wget --directory-prefix=~/cli_scripts https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/cli_scripts/$script_name
+    wget -O ~/cli_scripts/$script_name https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/cli_scripts/$script_name
 done
 echo 'export PATH=$PATH:~/cli_scripts' >> ~.bashrc
 echo 'chmod +x ~/cli_scripts/*' >> ~.bashrc
 
-wget --directory-prefix=~ https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.tmux.conf
+wget -O ~/.tmux.conf https://raw.githubusercontent.com/J-sephB-lt-n/my-personal-configs/main/.tmux.conf
 
 source ~/.bashrc
